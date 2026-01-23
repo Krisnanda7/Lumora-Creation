2b9d0359f5253a4e7af8500f986d84c731475737
Krisnanda <krisnanda@example.com>
2026-01-23 21:35:33 +0800

    fix error, update some features

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,5 @@
 import "./globals.css";
 import type { Metadata } from "next";
-import { useEffect } from "react";
 import AOSProvider from "@/components/AOSProvider";
 import { Analytics } from "@vercel/analytics/react";
 import { SpeedInsights } from "@vercel/speed-insights/next";
